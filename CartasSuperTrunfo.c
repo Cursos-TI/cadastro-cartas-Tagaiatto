23d1682d0fd1db56adecd29994ddde564997659d
Edilson Mendes <edilsonmneto@gmail.com>
2025-10-27 17:14:03 +0000

    criacao da estrutura de entrada de dados das duas cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,36 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-
+  int populacao1, populacao2;
+  float area1, area2;
+  float pib1, pib2;
+  int numPtTuristicos1, numPtTuristicos2;
   // Área para entrada de dados
+  printf("Digite a populacao da primeira carta: ");
+  scanf("%d", &populacao1);
 
+  printf("\nDigite a area da primeira carta: ");
+  scanf("%f", &area1);
+
+  printf("\nDigite o pib da primeira carta: ");
+  scanf("%f", &pib1);
+
+  printf("\nDigite o numero de pontos turisticos da primeira carta: ");
+  scanf("%d", &numPtTuristicos1);
+
+  printf("\nDigite a populacao da segunda carta: ");
+  scanf("%d", &populacao2);
+
+  printf("\nDigite a area da segunda carta: ");
+  scanf("%f", &area2);
+
+  printf("\nDigite o pib da segunda carta: ");
+  scanf("%f", &pib2);
+
+  printf("\nDigite o numero de pontos turisticos da segunda carta: ");
+  scanf("%d", &numPtTuristicos2);
   // Área para exibição dos dados da cidade
+  
 
 return 0;
 } 
